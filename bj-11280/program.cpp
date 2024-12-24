0eeed11a5e76d357eb2deea44d56993871517013
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-25 04:29:34 +0900

    *: solved

--- bj-11280/program.cpp
+++ bj-11280/program.cpp
@@ -1,59 +1,85 @@
+// https://www.acmicpc.net/source/85699032
+
+#include <algorithm>
 #include <iostream>
 #include <stack>
-#include <string>
 #include <vector>
 
 using namespace std;
 
 #define N_MAX 10000
 #define M_MAX 100000
-
+#define V_MAX (N_MAX * 2 + 1)
+#define min(x, y) (x) < (y) ? (x) : (y)
 #define NOT(x) (n + (x) - 1) % (n * 2) + 1
 
-int n, m;
-vector<int> adj[N_MAX * 2 + 1];
-int order[N_MAX * 2 + 1];
-int cur_order = 0;
-vector<int> group;
-vector<vector<int> > groups;
+vector<vector<int> > sccs;
+int num[V_MAX];
+int low[V_MAX];
+bool working[V_MAX];
+stack<int> s;
+int id;
 
-bool working[N_MAX * 2 + 1];
-bool visited[N_MAX * 2 + 1];
+int n, m;
+vector<int> adj[V_MAX];
 
 int inp_a, inp_b;
 
 int group_cnt = 0;
-int group_label[N_MAX * 2 + 1];
+int group_label[V_MAX];
 
-void dfs(int cur) {
-  visited[cur] = true;
-  working[cur] = true;
+void dfs(int from) {
+  num[from] = id++;
+  low[from] = num[from];
 
-  int original_order = cur_order;
-  order[cur] = cur_order;
-  cur_order++;
+  s.push(from);
+  working[from] = true;
 
-  for (int next : adj[cur]) {
-    if (!visited[next]) {
-      dfs(next);
-    }
-    if (working[next]) {
-      order[cur] = min(order[next], order[cur]);
+  for (auto to : adj[from]) {
+    if (!num[to]) {
+      dfs(to);
+      low[from] = min(low[from], low[to]);
+    } else if (working[to]) {
+      low[from] = min(low[from], num[to]);
     }
   }
 
-  group.push_back(cur);
+  if (num[from] == low[from]) {
+    vector<int> scc = vector<int>();
 
-  // cout << "a " << cur << " " << original_order << " " << order[cur] << endl;
-  if (original_order == order[cur]) {
-    for (auto x: group) {
-      group_label[x] = group_cnt;
+    int top;
+    while (true) {
+      top = s.top();
+      scc.push_back(top);
+      // sort(scc.begin(), scc.end());
+
+      s.pop();
+      working[top] = false;
+
+      if (top == from) {
+        break;
+      }
     }
-    group_cnt++;
-    group.clear();
+
+    sccs.push_back(scc);
+  }
+}
+
+// bool sccs_comp(vector<int> v, vector<int> w) { return v.at(0) < w.at(0); }
+
+void get_sccs() {
+  for (int i = 1; i <= n * 2; i++) {
+    num[i] = 0;
+    low[i] = 0;
+    working[i] = false;
+    id = 1;
   }
 
-  working[cur] = false;
+  for (int i = 1; i <= n * 2; i++) {
+    if (!num[i]) dfs(i);
+  }
+
+  // sort(sccs.begin(), sccs.end(), sccs_comp);
 }
 
 int main() {
@@ -72,18 +98,7 @@ int main() {
     adj[NOT(inp_b)].push_back(inp_a);
   }
 
-  for (size_t i = 1; i <= n * 2; i++) {
-    visited[i] = false;
-    working[i] = false;
-  }
-
-  for (size_t i = 1; i <= n * 2; i++) {
-    if (visited[i]) continue;
-
-    // cout << "[" << i << "]" << endl;
-
-    dfs(i);
-  }
+  get_sccs();
 
   // for (size_t i = 1; i <= n * 2; i++) {
   //   cout << i << "] ";
@@ -99,6 +114,13 @@ int main() {
   // {
   //   cout << i << "] " << group_label[i] << endl;
   // }
+
+  for (auto scc : sccs) {
+    for (auto x : scc) {
+      group_label[x] = group_cnt;
+    }
+    group_cnt++;
+  }
 
   bool ok = true;
   for (size_t i = 1; i <= n; i++)
