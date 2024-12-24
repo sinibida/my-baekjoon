f4f6ba27674db6a8b7af22aba7d333d2cd9a9331
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-25 03:07:09 +0900

    +: solved in theory

--- bj-11280/README.md
+++ bj-11280/README.md
@@ -1,1 +1,5 @@
 https://www.acmicpc.net/problem/11280
+
+1. scc사용
+
+`group-check` 배열을 활용해서 모순 발견
--- bj-11280/program.cpp
+++ bj-11280/program.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define N_MAX 10000
 #define M_MAX 100000
 
-#define NOT(x) (inp_a + (x) - 1) % (n * 2) + 1
+#define NOT(x) (n + (x) - 1) % (n * 2) + 1
 
 int n, m;
 vector<int> adj[N_MAX * 2 + 1];
@@ -23,6 +23,8 @@ bool visited[N_MAX * 2 + 1];
 int ret;
 
 int inp_a, inp_b;
+
+bool group_check[N_MAX * 2 + 1];
 
 void dfs(int cur) {
   visited[cur] = true;
@@ -42,6 +44,7 @@ void dfs(int cur) {
 
   group.push_back(cur);
 
+  //cout << "a " << cur << " " << original_order << " " << order[cur] << endl;
   if (original_order == order[cur]) {
     groups.push_back(group);
     group.clear();
@@ -62,17 +65,14 @@ int main() {
     if (inp_a < 0) inp_a = -inp_a + n;
     if (inp_b < 0) inp_b = -inp_b + n;
 
-    adj[inp_a].push_back(inp_b);
-    // adj[NOT(inp_a)].push_back(inp_b);
-    // adj[NOT(inp_b)].push_back(inp_a);
+    adj[NOT(inp_a)].push_back(inp_b);
+    adj[NOT(inp_b)].push_back(inp_a);
   }
-
+  
   for (size_t i = 1; i <= n * 2; i++) {
     visited[i] = false;
     working[i] = false;
   }
-
-  ret = 0;
 
   for (size_t i = 1; i <= n * 2; i++) {
     if (visited[i]) continue;
@@ -84,19 +84,44 @@ int main() {
     ret++;
   }
 
-  for (size_t i = 1; i <= n * 2; i++) {
-    for (auto x : adj[i]) cout << x << " ";
-    cout << endl;
+  // for (size_t i = 1; i <= n * 2; i++) {
+  //   for (auto x : adj[i]) cout << x << " ";
+  //   cout << endl;
+  // }
+
+  // for (size_t i = 1; i <= n * 2; i++) {
+  //   cout << i << ": " << order[i] << endl;
+  // }
+
+  // for (auto group : groups) {
+  //   for (auto x : group) cout << x << " ";
+  //   cout << endl;
+  // }
+
+  bool ok = true;
+  for (vector<int> group : groups) {
+    for (auto x : group) {
+      int at = x;
+      if (at > n) at = NOT(at);
+
+      if (group_check[at]) {
+        ok = false;
+        break;
+      }
+      group_check[at] = true;
+    }
+
+    if (!ok) {
+      break;
+    }
+
+    for (auto x : group) {
+      int at = x;
+      if (at > n) at = NOT(at);
+
+      group_check[at] = false;
+    }
   }
 
-  for (size_t i = 1; i <= n * 2; i++) {
-    cout << i << ": " << order[i] << endl;
-  }
-
-  for (auto group : groups) {
-    for (auto x : group) cout << x << " ";
-    cout << endl;
-  }
-
-  // cout << ret << endl;
+  cout << (ok ? '1' : '0') << endl;
 }
