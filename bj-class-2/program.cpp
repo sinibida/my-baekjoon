24b787e917ab9bc4de57a91c20973afaa0d6cce1
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-28 07:42:51 +0900

    +: 1920 try:

--- bj-class-2/program.cpp
+++ bj-class-2/program.cpp
@@ -3,9 +3,32 @@
 
 using namespace std;
 
+int n, m;
+int narr[100000], marr[100000];
+
 int main() {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  cout << "Hello World!" << endl;
+  cin >> n;
+  for (size_t i = 0; i < n; i++) {
+    cin >> narr[i];
+  }
+
+  cin >> m;
+  for (size_t i = 0; i < m; i++) {
+    cin >> marr[i];
+  }
+
+  for (size_t i = 0; i < m; i++) {
+    int found = false;
+    for (size_t j = 0; j < n; j++) {
+      if (marr[i] == narr[j]) {
+        found = true;
+        break;
+      }
+    }
+
+    cout << found << '\n';
+  }
 }
