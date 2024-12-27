9b2c81dd8a3e0435988142e86ce37ee1d846907c
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-28 08:37:08 +0900

    2798 checkpoint

--- bj-class-2/program.cpp
+++ bj-class-2/program.cpp
@@ -5,6 +5,40 @@
 using namespace std;
 
 int n, m;
+int arr[100];
+int x;
+int ans;
+int cnt;
+
+void fn(int from) {
+  x += arr[from];
+  cnt++;
+
+  if (x > m) {
+    x -= arr[from];
+    return;
+  }
+
+  cout << from << "\t" << x << endl;
+  ans = max(ans, x);
+
+  for (size_t i = from + 1; i < n; i++) {
+    fn(i);
+  }
+
+  x -= arr[from];
+  cnt--;
+}
+
+void fn_start() {
+  x = 0;
+  cnt = 0;
+  ans = -1;
+
+  for (size_t i = 0; i < n; i++) {
+    fn(i);
+  }
+}
 
 int main() {
   cin.tie(NULL);
@@ -12,16 +46,11 @@ int main() {
 
   cin >> n >> m;
 
-  int nn = n;
-  int mm = m;
-  int temp;
-  while (mm > 0) {
-    temp = mm;
-    mm = nn % mm;
-    nn = temp;
+  for (size_t i = 0; i < n; i++) {
+    cin >> arr[i];
   }
 
-  int gcd = nn;
-  int lcm = n / gcd * m;
-  cout << gcd << endl << lcm << endl;
+  fn_start();
+
+  cout << ans << endl;
 }
